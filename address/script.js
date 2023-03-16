48ad66d9b95848ff459a2e66e5ed9feb5cf42d86
Katsu <messyaikei456@gmail.com>
2023-03-17 00:08:39 +0530

    up

---
+++ address/script.js
@@ -0,0 +1,12 @@
+window.onload=function() {
+    var press= document.getElementById("addaddress");
+    var pressadd=document.getElementById("adding");
+    var presscancel= document.getElementById("cancel");
+    var addform=document.querySelector('.form');
+
+    var fname= document.getElementById("fullname");
+    var phn= document.getElementById("phone");
+    var address= document.getElementById("add");
+    
+    
+}
