4bc55511bf346bbcc7f2bcfb8498c7ccc4e7a240
Katsu <messyaikei456@gmail.com>
2023-03-17 12:49:10 +0530

    up

--- address/script.js
+++ address/script.js
@@ -7,6 +7,29 @@ window.onload=function() {
     var fname= document.getElementById("fullname");
     var phn= document.getElementById("phone");
     var address= document.getElementById("add");
+    var book= document.querySelector(".book");
+    var address=[];
     
-    
+    press.addEventListener("click",function() {
+        addform.style.display= "block";
+
+        
+    })
+    pressadd.addEventListener("click",addbook);
+    book.addEventListener("click",remove);
+    function jsonfile(fullname,address,phnno){
+        this.fullname=fullname;
+        this.address=address;
+        this.phnno=phnno;
+
+    }
+    function addbook() {
+        var isnull=fullname.value!='' && phone.value!='' && address.value!='' && city.value!='' && email.value!='';
+        if(isnull){
+            
+        }
+
+        
+    }
+
 }
