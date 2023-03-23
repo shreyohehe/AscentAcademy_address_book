21cad9db956bf690a0c071be1d5302c6cecd3aaf
Katsu <messyaikei456@gmail.com>
2023-03-23 19:48:18 +0530

    up

--- address/script.js
+++ address/script.js
@@ -4,12 +4,12 @@ window.onload = function () {
     var presscancel = document.getElementById("cancel");
     var addform = document.querySelector('.form');
 
-    var fname = document.getElementById("fullname");
-    var phn = document.getElementById("phone");
-    var addrss = document.getElementById("add");
-    var cite = document.getElementById("city");
+    var fullname = document.getElementById("fullname");
+    var phnno = document.getElementById("phone");
+    var address = document.getElementById("add");
+    var city = document.getElementById("city");
     var book = document.querySelector(".book");
-    var addressb = [];
+   
 
     press.addEventListener("click", function () {
         addform.style.display = "block";
@@ -20,6 +20,9 @@ window.onload = function () {
         addform.style.display = "none";
 
     })
+    pressadd.addEventListener("click", addbook);
+    book.addEventListener("click", remove);
+    var addressb = [];
 
     function jsonfile(fullname, address, phnno, city) {
         this.fullname = fullname;
@@ -29,9 +32,9 @@ window.onload = function () {
 
     }
     function addbook() {
-        var isnull = fname.value != '' && phn.value != '' && addrss.value != '' && cite.value != '';
+        var isnull = fullname.value != '' && phnno != '' && address.value != '' && city.value != '';
         if (isnull) {
-            var obj = new jsonfile(fname.value, addrss.value, phn.value, cite.value);
+            var obj = new jsonfile(fullname.value, address.value, phnno.value, city.value);
             addressb.push(obj);
             localStorage['addressbook'] = JSON.stringify(addressb);
             addform.style.display = "none";
@@ -44,7 +47,20 @@ window.onload = function () {
 
 
     }
-    pressadd.addEventListener("click", addbook);
+    function remove(e) {
+        if (e.target.classList.contains("delbut")) {
+            var aid = e.target.getAttribute('data-id');
+            addressb.splice(aid, 1);
+            localStorage['addressbook'] = JSON.stringify(addressb);
+            showbook();
+
+
+
+        }
+
+
+    }
+   
     function clear() {
         var fields = document.querySelectorAll('.fields');
         for (var i in fields) {
@@ -53,19 +69,19 @@ window.onload = function () {
 
     }
     function showbook() {
-        if (localStorage['addressbook'] == undefined) {
+        if ( localStorage['addressbook'] == undefined) {
             localStorage['addressbook'] = '';
         }
         else {
-            addressb = JSON.parse(localStorage['addressbook']);
+            addressb = JSON.parse( localStorage['addressbook']);
             book.innerHTML = '';
-            for (var j in addressb) {
+            for (var j in  addressb ) {
                 var s = '<div class="entry">'
                 s += '<div class="name"><p>' + addressb[j].fullname + '</p></div>';
-                s += '<div class="addressppl"><p>' + addressb[j].address + '</p></div>';
-                s += '<div class="cit"><p>' + addressb[j].city + '</p></div>';
+                s += '<div class="address"><p>' + addressb[j].address + '</p></div>';
+                s += '<div class="city"><p>' + addressb[j].city + '</p></div>';
                 s += '<div class="phone"><p>' + addressb[j].phnno + '</p></div>';
-                s += '<div class="delete"><a class="delbut" href="#" data-id="' + j + '">Delete</a></div>';
+                s += '<div class="del"><a class="delbut" href="#" data-id="' + j + '">Delete</a></div>';
                 s += '</div>';
                 book.innerHTML += s;
 
@@ -76,20 +92,8 @@ window.onload = function () {
 
     }
 
-    function remove(e) {
-        if (e.target.classList.contains("delbut")) {
-            var id = e.target.getAttribute('data-id');
-            addressb.splice(id, 1);
-            localStorage['addressbook'] = JSON.stringify(addressb);
-            showbook();
-
-
-
-        }
-
-
-    }
-    book.addEventListener("click", remove);
+    
+   
     showbook();
 
 
